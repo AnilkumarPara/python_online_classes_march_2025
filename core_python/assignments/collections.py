1601ac270a0dd6e53be0d9a45841091d951e91be
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2025-04-14 15:28:31 +0530

    Create collections.py

---
+++ core_python/assignments/collections.py
@@ -0,0 +1,38 @@
+# Creation
+# 1. Create a tuple with 5 different fruits and access the 3rd fruit using the negative indexing and also Find the length of the tuple
+# 2. Create a set of 5 unique colors
+# 3. Create a frozen set of 5 unique numbers
+# 4. Create a nested list with 3 lists, each containing 3 integers
+# 5. Create a dictionary with 5 key-value pairs, where keys are student names and values are their grades
+
+# Indexing
+# 6. Modify third element to 300 using indexing
+my_list = [10, 20, 30, 40, 50]
+
+# 7. Retrieve the last element using indexing
+my_tuple = (10, 20, 30, 40, 50)
+
+
+# 8. Access the 5 using negative indexing
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+# 9. Access the 8 using positive indexing
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+#  10. Modify element 3 with 4000
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+# 11. Access the Grade of Alice
+grades = {"Alice": 85, "Bob": 90, "Charlie": 78, "David": 92, "Eve": 88}
+
+# 12. Modify David grade to 99
+grades = {"Alice": 85, "Bob": 90, "Charlie": 78, "David": 92, "Eve": 88}
+
+
+
+
+
+
+
