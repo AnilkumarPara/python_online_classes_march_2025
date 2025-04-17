f7fd4675c2fc37b0d4299181f54d562d0fc22147
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2025-04-17 18:48:36 +0530

    Add files via upload

---
+++ core_python/assignments/dictionary_assignments.py
@@ -0,0 +1,21 @@
+"""
+Assignment-1:
+WAP to create a one student info in dictionary. Dictionary should contain
+'student_name, age, roll_no, class, section, percentage' student data.
+Retrieve the student percentage from the dictionary.
+"""
+
+"""
+Assignment-2:
+WAP to create a 3 students info in dictionary. Dictionary should contain 3 students data with 'student_names,
+ages, roll_nos, classes, sections, percentages' keys and values can be stored in list/tuple.
+Retrieve the student_3 class from the dictionary.
+"""
+
+"""
+Assignment-3:
+WAP to create a 4 employees data in a nested dictionary.
+Dictionary should contain 4 employees data, each employee data should be represented in a dictionary
+Each sub dictionary should have 'employee_name,salary,Designation' keys.
+Retrieve the 3rd employee designation from the nested dictionary.
+"""
---
+++ core_python/assignments/slicing_assignments.py
@@ -0,0 +1,43 @@
+# Retrieve 'nA' from the following tuple using indexing and slicing
+t = ([30, 'hi', (4, {'names': ['Anil', 'Sunil'], 'Ages': (45, 47)})])
+
+# WAP to retrieve ' OE' in reverse order using the positive indexing from following string
+s1 = 'I LOVE PYTHON'
+
+# WAP to extract 'Hyderabad' in reverse order using negative indexing from following string
+s2 = 'Hello I am going to Hyderabad How are you doing?'
+
+# WAP to print the complete string in 7 ways using the slicing. String is given below
+s3 = 'Programming'
+
+# WAP to retrieve the 'Iam' from following string using slicing
+s4 = 'I ama jam'
+
+"""
+WAP to retrieve 'Python' in reverse order using negative indexing.
+You should use Indexing and slicing Together. Please use below list.
+"""
+l1 = [1, 7.3, [33, 22], 'Hello Python']
+
+"""
+WAP to retrieve Jayansh and Shanvika ages in reverse order using positive indexing.
+Output should be [7,4].
+Please use below dictionary.
+"""
+students_info = {'student_names': ['Anil', 'Jayansh', 'Shanvika'], 'ages': [19, 4, 7],
+                 'roll_nos': (201, 202, 205), 'classes': ['Intermediate', 'UKG', '2nd Grade'],
+                 'sections': ['A', 'E', 'G'], 'percentages': [65.6, 78.9, 99.1]
+                 }
+"""
+WAP to retrieve (4,5) using positive indexing.
+You should use Indexing and slicing Together.
+Please use below list.
+"""
+l2 = [21, ['hi', 'hello', (3, 4, 5)], 45, 765, 2001, 51, 2034, 'Jai']
+
+"""
+Retrieve the value 2 using indexing and slicing using positive indexing.
+Please use below list.
+Write down the differences.
+"""
+l3 = [1, 2, 3]
