a6361a6bef596d04eebaa6b9aea8507939bb8bc5
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2025-05-05 19:15:28 +0530

    Add files via upload

---
+++ core_python/assignments/control_statements/conditional/conditional_statements_assignments.py
@@ -0,0 +1,36 @@
+# Assignment-1: if condition
+# Check if a person is eligible to vote based on their age
+
+# Input: Age of the person
+
+# Check if the person is eligible to vote
+
+# Assignment-2: if else condition
+# WAP that checks whether a number is positive or negative
+
+# Assignment-3: if else condition
+# WAP that Checks if a given number is even or odd
+
+# Assignment-4: nested if condition
+# WAP to find the greatest of 3 numbers
+
+# Assignment-5: nested if else condition
+"""
+
+Movie Ticket Pricing
+Imagine a movie theater that offers different ticket prices based on the age of the customer and the time of the day. The rules might be as follows:
+
+Regular price: $10
+Children under 12 and seniors over 65 get a 50% discount.
+Matinee show (before 5 PM) offers a 25% discount to all.
+"""
+
+# Assignment-6: nested if else condition
+# WAP to find the biggest country among 3 based on the population
+
+# Assignment-7:
+"""
+WAP that takes a Username and Password as input, Checks if the user's
+credentials match a predefined Username and Password .
+If they match, display success message; otherwise display a failure message.
+"""
