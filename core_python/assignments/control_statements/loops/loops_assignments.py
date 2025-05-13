6a2f63d8fa7ace79cf21c74e0e4c2a2e633ebac3
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2025-05-13 21:05:19 +0530

    Add files via upload

---
+++ core_python/assignments/control_statements/loops/loops_assignments.py
@@ -0,0 +1,26 @@
+"""
+Count Occurrences: Given a list and a target element,
+count the occurrences of the target in the list.
+"""
+numbers = [1, 2, 3, 4, 5, 2, 2, 6, 7]
+target = int(input("Enter the number to find the number of occurrence "))
+
+
+# Count and display vowels in a string
+s1 = input("Enter the string to find the vowels in a string ")
+
+# Calculate the factorial of a number
+n = int(input("Enter the number to calculate the factorial "))
+
+"""
+A palindrome number is a number that remains the same when its digits are reversed.
+For example, 121, 1331, 16461 are palindrome numbers, while 123 and 321 are not.
+"""
+
+# WAP to unlock the mobile device using a pattern
+"""
+User enters a correct password in 5 attempts, phone will be unlocked.
+If a user enters an incorrect password for 5 attempts,
+You have to wait for 30 seconds to try again.
+This process repeats
+"""
