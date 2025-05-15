f9347ea4806d047be30cfff3a7a72eb794a482d4
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2025-05-15 22:56:35 +0530

    Add files via upload

---
+++ core_python/assignments/control_statements/loops/nested_loops/pattern_programs_assignments.py
@@ -0,0 +1,35 @@
+# WAP to print the squared number pattern
+"""
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+"""
+
+# WAP to print the squared number pattern
+"""
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+"""
+
+# WAP to print the Right Angled Triangle pattern
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
+
+# Inverted Right-Angled Triangle Number Pattern
+"""
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+"""
