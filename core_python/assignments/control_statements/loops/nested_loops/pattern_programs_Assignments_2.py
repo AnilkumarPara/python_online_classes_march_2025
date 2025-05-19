7cca74fc9dfb1f64ced32229b22081b923643a51
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2025-05-19 19:47:19 +0530

    Add files via upload

---
+++ core_python/assignments/control_statements/loops/nested_loops/pattern_programs_Assignments_2.py
@@ -0,0 +1,52 @@
+"""
+Floyd's triangle pattern
+The Floyd's triangle is a right-angled triangle that contains
+consecutive natural numbers, starting with a 1 in the top left corner.
+"""
+"""
+1
+2 3
+4 5 6
+7 8 9 10
+"""
+
+# Checkerboard Pattern
+"""
+The pattern typically contains two colours where a single checker
+(that is a single square within the check pattern) is surrounded
+on all four sides by a checker of a different colour.
+"""
+"""
+B W B W
+W B W B
+B W B W
+W B W B
+"""
+
+
+# Hollow Square star pattern
+"""
+* * * *
+*     *
+*     *
+* * * *
+"""
+
+# Pyramid Star Pattern
+"""
+   *
+  ***
+ *****
+*******
+"""
+
+# Diamond Pattern
+"""
+    *
+   ***
+  *****
+ *******
+  *****
+   ***
+    *
+"""
