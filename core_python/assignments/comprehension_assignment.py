847d304f43db5e52ca176cb6aaf5ae903bd9e0ee
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2025-06-22 19:36:02 +0530

    Add files via upload

---
+++ core_python/assignments/comprehension_assignment.py
@@ -0,0 +1,31 @@
+"""
+WAP to find names starts with ‘A’ and ends with 'a'
+in the given list of names using list comprehension
+"""
+names = ['Abeesha', 'Abelia', 'Anil', 'Shanvika', 'Jayansh']
+
+"""
+WAP to extract each word from a sentence and store all the words
+in the form of list using list comprehension
+"""
+
+paragraph = ["There was a fox.", 'It was brown in color.', "It was seen near that farm sometime back"]
+
+# Output
+['There', 'was', 'a', 'fox.', 'It', 'was', 'brown', 'in', 'color.', 'It', 'was', 'seen', 'near', 'that', 'farm', 'sometime', 'back']
+
+"""
+Count the frequency of each fruit in a list and output as
+key: value
+fruit:count
+Write program using dictionary comprehension
+"""
+fruits = ["apple", "banana", "apple", "cherry", "banana"]
+
+"""
+create dictionary with string of lengths and output as
+key: value
+word:length
+Write program using dictionary comprehension
+"""
+words = ["hey", "Hello", "Covid"]
